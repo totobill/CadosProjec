85026315220a6bf380ea1923a5f00c8a2ecd6fa7
totobill <antho.rohr@yahoo.fr>
2017-09-17 16:21:05 +0200

    modification profil

--- data/genere/dev-cados/module/configuration/main.php
+++ data/genere/dev-cados/module/configuration/main.php
@@ -35,6 +35,49 @@ class module_configuration extends abstract_module{
             $this->oLayout->add('main',$oView);
         }
         
+         private function processSave(){
+        
+            if(!_root::getRequest()->isPost() ){ //si ce n'est pas une requete POST on ne soumet pas
+                    return null;
+            }
+
+            $oPluginXsrf=new plugin_xsrf();
+            if(!$oPluginXsrf->checkToken( _root::getParam('token') ) ){ //on verifie que le token est valide
+                return array('token'=>$oPluginXsrf->getMessage() );
+            }
+
+            $iId=_root::getParam('id',null);
+            if($iId==null){
+                $oUtilisateur=new row_utilisateur;	
+            }else{
+                $oUtilisateur=model_utilisateur::getInstance()->findById( _root::getParam('id',null) );
+            }
+
+            $sPassword=_root::getParam('password',null);
+            $sConfirmationPassword=_root::getParam('confirmationPassword',null);
+            if($sConfirmationPassword!=null){
+                if(strcmp($sPassword, $sConfirmationPassword) != 0){
+                    return array('password' => 'Le mot de passe et la confirmation ne sont pas identiques');
+                }
+            }
+
+            $tColumn=array('nom','prenom','date_de_naissance','numero','email','pseudo');
+            foreach($tColumn as $sColumn){
+                $oUtilisateur->$sColumn=_root::getParam($sColumn,null) ;
+            }
+
+            $oUtilisateur->password=model_utilisateur::getInstance()->hashPassword($sPassword);
+
+            if($oUtilisateur->saveModification()){
+                //une fois enregistre on redirige (vers la page profil)
+                _root::getAuth()->setAccount($oUtilisateur);
+                _root::redirect('configuration::profil');
+            }else{
+                return $oUtilisateur->getListErrorModification();
+            }
+
+        }
+        
         public function _modifierPassword(){
 
             $tMessage=$this->processSavePassword();
