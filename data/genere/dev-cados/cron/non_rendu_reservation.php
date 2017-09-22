dc39d1bfd2a27f358d6b84ba668183cdfbfa88d1
118454 <u88760823@cbi-bs1.server.lan>
2017-09-22 23:18:52 +0200

    modification non_rendu_reservation

--- data/genere/dev-cados/cron/non_rendu_reservation.php
+++ data/genere/dev-cados/cron/non_rendu_reservation.php
@@ -1,16 +1,19 @@
 <?php
+
 $username = 'dbo675117633';
 $password = 'cadosAdmin';
-$bdd = new PDO('mysql:dbname=db675117633;host=db675117633.db.1and1.com', $username, $password);
-$oUserUndelivery = $bdd->query('SELECT * FROM utilisateur WHERE id_bouton > 0');
+try{	
+	$bdd = new PDO('mysql:dbname=db675117633;host=db675117633.db.1and1.com', $username, $password);
+	$oUserUndelivery = $bdd->query('SELECT * FROM utilisateur WHERE id_bouton > 0');
+	foreach ($oUserUndelivery as $oUser){
+		var_dump($oUser);
+		$nbr_jour_reservation =(int) ++$oUser['nbr_jour_reservation'];
+		$id_utilisateur = (int) $oUser['id_utilisateur'];
+		$requete = $bdd->prepare('UPDATE utilisateur SET nbr_jour_reservation = :nbrjour WHERE id_utilisateur = :id');
+		$requete->execute(array('nbrjour' => $nbr_jour_reservation, 'id' => $id_utilisateur));
+	}
+}catch(Exception $e){
 
-foreach ($oUserUndelivery as $oUser){
-    $oUser->nbr_jour_reservation += 1;
-    
-    if($oUser->save()){
-        
-    }else{
-        echo 'erreur';
-    }
+	die('Erreur :'.$e->getMessage());
+
 }
-
