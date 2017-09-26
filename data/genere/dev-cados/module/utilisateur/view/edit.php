248d8aa37420c7e3bdfe9710da8d7358817d838d
totobill <antho.rohr@yahoo.fr>
2017-09-26 23:17:16 +0200

    changement du tableau utilisateurs

--- data/genere/dev-cados/module/utilisateur/view/edit.php
+++ data/genere/dev-cados/module/utilisateur/view/edit.php
@@ -6,48 +6,48 @@ $oForm->setMessage($this->tMessage);
 <form class="form-horizontal" action="" method="POST" >
 	
 	<div class="form-group">
-		<label class="col-sm-2 control-label">Nom</label>
-		<div class="col-sm-10"><?php echo $oForm->getInputText('nom',array('class'=>'form-control')) ?></div>
+		<label class="col-sm-4 control-label">Nom</label>
+		<div class="col-sm-8"><?php echo $oForm->getInputText('nom',array('class'=>'form-control')) ?></div>
 	</div>
 		
 	<div class="form-group">
-		<label class="col-sm-2 control-label">Prénom</label>
-		<div class="col-sm-10"><?php echo $oForm->getInputText('prenom',array('class'=>'form-control')) ?></div>
+		<label class="col-sm-4 control-label">Prénom</label>
+		<div class="col-sm-8"><?php echo $oForm->getInputText('prenom',array('class'=>'form-control')) ?></div>
 	</div>
 		
 	<div class="form-group">
-		<label class="col-sm-2 control-label">Date de naissance</label>
-		<div class="col-sm-10"><?php echo $oForm->getInputDate('date_de_naissance',array('class'=>'form-control')) ?></div>
+		<label class="col-sm-4 control-label">Date de naissance</label>
+		<div class="col-sm-8"><?php echo $oForm->getInputDate('date_de_naissance',array('class'=>'form-control')) ?></div>
 	</div>
 			
 	<div class="form-group">
-		<label class="col-sm-2 control-label">Numéro</label>
-		<div class="col-sm-10"><?php echo $oForm->getInputText('numero',array('class'=>'form-control')) ?></div>
+		<label class="col-sm-4 control-label">Numéro</label>
+		<div class="col-sm-8"><?php echo $oForm->getInputText('numero',array('class'=>'form-control')) ?></div>
 	</div>
 		
 	<div class="form-group">
-		<label class="col-sm-2 control-label">Email</label>
-		<div class="col-sm-10"><?php echo $oForm->getInputEmail('email',array('class'=>'form-control')) ?></div>
+		<label class="col-sm-4 control-label">Email</label>
+		<div class="col-sm-8"><?php echo $oForm->getInputEmail('email',array('class'=>'form-control')) ?></div>
 	</div>
 		
 	<div class="form-group">
-		<label class="col-sm-2 control-label">Pseudo</label>
-		<div class="col-sm-10"><?php echo $oForm->getInputText('pseudo',array('class'=>'form-control')) ?></div>
+		<label class="col-sm-4 control-label">Pseudo</label>
+		<div class="col-sm-8"><?php echo $oForm->getInputText('pseudo',array('class'=>'form-control')) ?></div>
 	</div>
 		
 	<div class="form-group">
-		<label class="col-sm-2 control-label">Mot de passe</label>
-		<div class="col-sm-10"><?php echo $oForm->getInputPassword('password',array('class'=>'form-control')) ?></div>
+		<label class="col-sm-4 control-label">Mot de passe</label>
+		<div class="col-sm-8"><?php echo $oForm->getInputPassword('password',array('class'=>'form-control')) ?></div>
 	</div>
 		
 	<div class="form-group">
-		<label class="col-sm-2 control-label">Casier réservé (0 = Aucun)</label>
-		<div class="col-sm-10"><?php echo $oForm->getInputText('id_bouton',array('class'=>'form-control')) ?></div>
+		<label class="col-sm-4 control-label">Casier réservé (0 = Aucun)</label>
+		<div class="col-sm-8"><?php echo $oForm->getInputText('id_bouton',array('class'=>'form-control')) ?></div>
 	</div>
 		
 	<div class="form-group">
-		<label class="col-sm-2 control-label">connecté ? (0 non connecté, 1 connecté)</label>
-		<div class="col-sm-10"><?php echo $oForm->getInputText('connecte',array('class'=>'form-control')) ?></div>
+		<label class="col-sm-4 control-label">connecté ? (0 non connecté, 1 connecté)</label>
+		<div class="col-sm-8"><?php echo $oForm->getInputText('connecte',array('class'=>'form-control')) ?></div>
 	</div>
 	
 	
