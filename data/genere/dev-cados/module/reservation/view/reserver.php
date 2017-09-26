a9e2c6796626e95271da8c4e7b660da912c32b44
totobill <antho.rohr@yahoo.fr>
2017-09-26 23:44:27 +0200

    changement du tableau utilisateurs

--- data/genere/dev-cados/module/reservation/view/reserver.php
+++ data/genere/dev-cados/module/reservation/view/reserver.php
@@ -4,17 +4,22 @@
         <?php foreach ($this->oCasier as $casier){ ?>
             <?php if ($casier->etat == 1) { ?>
                 <div class="reservation_reserver_casier col-xs-6 col-lg-2">
-                    <a href="#">
+                    <button class="casier_reserver" type="button" value='<?php echo $casier->numero; ?>' name="num_bouton">    
                         <img class="casier_reserver" src="css/images/casier_reserver.png" alt="Responsive image">
-                        <button class="casier_reserver" type="button" value='<?php echo $casier->numero; ?>' name="num_bouton"><?php echo $casier->numero; ?></button>
-                    </a>
+                        <?php echo $casier->numero; ?>
+                    </button>
+
                 </div>
             <?php }else{ ?>
                 <div class="reservation_reserver_casier col-xs-6 col-lg-2">
-                    <a href="#">
-                        <img class="casier_libre" src="css/images/casier_libre.png" alt="Responsive image">
-                        <input class="casier_libre" type="submit" value='<?php echo $casier->numero; ?>' name="num_bouton">
-                    </a>
+<!--                        <img class="casier_libre" src="css/images/casier_libre.png" alt="Responsive image">
+                        <button class="casier_libre" type="submit" value='//<?php //echo $casier->numero; ?>' name="num_bouton">-->
+                            
+                    <button class="casier_libre" type="submit" value='<?php echo $casier->numero; ?>' name="num_bouton">    
+                            <img class="casier_libre" src="css/images/casier_libre.png" alt="Responsive image">
+                            <?php echo $casier->numero; ?>
+                    </button>    
+                            
                 </div>
             <?php } ?> 
         <?php } ?>
