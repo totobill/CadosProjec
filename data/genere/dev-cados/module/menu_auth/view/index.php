7c4044bb2eab71f71c40f9ce7257d3cae78a3c04
totobill <antho.rohr@yahoo.fr>
2017-10-12 15:52:07 +0200

    modif barre menu

--- data/genere/dev-cados/module/menu_auth/view/index.php
+++ data/genere/dev-cados/module/menu_auth/view/index.php
@@ -7,7 +7,7 @@
 			<span class="icon-bar"></span>
 			<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="#">CADOS</a>
+			<a class="navbar-brand" href="#">CADOS AUTH</a>
 		</div>
 		<div class="collapse navbar-collapse">
 
