5b56ea598b973a03c5bad881a88ebbd20835ebbc
totobill <antho.rohr@yahoo.fr>
2017-11-10 11:11:37 +0100

    ajout du dossier de gmaps.js

--- data/genere/dev-cados/layout/bootstrap.php
+++ data/genere/dev-cados/layout/bootstrap.php
@@ -10,13 +10,10 @@
     <!-- Bootstrap -->
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-        <link rel="stylesheet" href="css/test.css" />
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
+    <script src="js/gmaps.js"></script>
+    <link rel="stylesheet" href="css/test.css" />
   </head>
   <body>
 	  
--- data/genere/dev-cados/module/map/view/index.php
+++ data/genere/dev-cados/module/map/view/index.php
@@ -1,21 +1,47 @@
-<div id="basic_map"></div>
-    <p>Bonjour</p>
-    <script type=text/javascript>    /**
+<div id="basic_map">
+    
+    <div id="mouse_event_map"></div>
+</div>
+
+<script>
+    /**
   * Basic Map
   */
-$(document).ready(function(){
- var map = new GMaps({
-    el: '#basic_map',
-    lat: 51.5073346,
-    lng: -0.1276831,
-    zoom: 12,
-    zoomControl : true,
-    zoomControlOpt: {
-        style : 'SMALL',
-        position: 'TOP_LEFT'
-    },
-    panControl : false
-  });
-});
-</script>
+    $(document).ready(function(){
+     var map = new GMaps({
+        el: '#basic_map',
+        lat: 51.5073346,
+        lng: -0.1276831,
+        zoom: 12,
+        zoomControl : true,
+        zoomControlOpt: {
+            style : 'SMALL',
+            position: 'TOP_LEFT'
+        },
+        panControl : false,
+      });
+    });
 
+    /**
+    * Mouse Events on Maps
+    */
+    $(document).ready(function(){
+     var map = new GMaps({
+        el: '#mouse_event_map',
+        lat: 51.5073346,
+        lng: -0.1276831,
+        zoom: 12,
+        zoomControl : true,
+        zoomControlOpt: {
+            style : 'SMALL',
+            position: 'TOP_LEFT'
+        },
+        panControl : false,
+        click: function(e){
+          alert('Click event');
+        },
+        dragend: function(e){
+          alert('Drag Event');
+        }
+      });
+</script>
