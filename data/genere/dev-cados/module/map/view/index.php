23d99a55e8b120883880f02d1a98829bee510c5e
totobill <antho.rohr@yahoo.fr>
2017-11-10 11:32:32 +0100

    modification liens js

--- data/genere/dev-cados/module/map/view/index.php
+++ data/genere/dev-cados/module/map/view/index.php
@@ -1,47 +1,79 @@
-<div id="basic_map">
-    
-    <div id="mouse_event_map"></div>
-</div>
 
-<script>
-    /**
-  * Basic Map
-  */
-    $(document).ready(function(){
-     var map = new GMaps({
-        el: '#basic_map',
-        lat: 51.5073346,
-        lng: -0.1276831,
-        zoom: 12,
-        zoomControl : true,
-        zoomControlOpt: {
-            style : 'SMALL',
-            position: 'TOP_LEFT'
-        },
-        panControl : false,
-      });
-    });
 
-    /**
-    * Mouse Events on Maps
-    */
-    $(document).ready(function(){
-     var map = new GMaps({
-        el: '#mouse_event_map',
-        lat: 51.5073346,
-        lng: -0.1276831,
-        zoom: 12,
-        zoomControl : true,
-        zoomControlOpt: {
-            style : 'SMALL',
-            position: 'TOP_LEFT'
+<div id="map"></div>
+
+    <script>
+      var customLabel = {
+        restaurant: {
+          label: 'R'
         },
-        panControl : false,
-        click: function(e){
-          alert('Click event');
-        },
-        dragend: function(e){
-          alert('Drag Event');
+        bar: {
+          label: 'B'
         }
-      });
-</script>
+      };
+
+        function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: new google.maps.LatLng(-33.863276, 151.207977),
+          zoom: 12
+        });
+        var infoWindow = new google.maps.InfoWindow;
+
+          // Change this depending on the name of your PHP or XML file
+          downloadUrl('https://storage.googleapis.com/mapsdevsite/json/mapmarkers2.xml', function(data) {
+            var xml = data.responseXML;
+            var markers = xml.documentElement.getElementsByTagName('marker');
+            Array.prototype.forEach.call(markers, function(markerElem) {
+              var name = markerElem.getAttribute('name');
+              var address = markerElem.getAttribute('address');
+              var type = markerElem.getAttribute('type');
+              var point = new google.maps.LatLng(
+                  parseFloat(markerElem.getAttribute('lat')),
+                  parseFloat(markerElem.getAttribute('lng')));
+
+              var infowincontent = document.createElement('div');
+              var strong = document.createElement('strong');
+              strong.textContent = name
+              infowincontent.appendChild(strong);
+              infowincontent.appendChild(document.createElement('br'));
+
+              var text = document.createElement('text');
+              text.textContent = address
+              infowincontent.appendChild(text);
+              var icon = customLabel[type] || {};
+              var marker = new google.maps.Marker({
+                map: map,
+                position: point,
+                label: icon.label
+              });
+              marker.addListener('click', function() {
+                infoWindow.setContent(infowincontent);
+                infoWindow.open(map, marker);
+              });
+            });
+          });
+        }
+
+
+
+      function downloadUrl(url, callback) {
+        var request = window.ActiveXObject ?
+            new ActiveXObject('Microsoft.XMLHTTP') :
+            new XMLHttpRequest;
+
+        request.onreadystatechange = function() {
+          if (request.readyState == 4) {
+            request.onreadystatechange = doNothing;
+            callback(request, request.status);
+          }
+        };
+
+        request.open('GET', url, true);
+        request.send(null);
+      }
+
+      function doNothing() {}
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCT3WwQTy1MpdhHX1IJfrDA1KVaQspjVy8&callback=initMap">
+    </script>
