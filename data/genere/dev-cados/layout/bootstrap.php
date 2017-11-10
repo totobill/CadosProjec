774631eb06dd51067ce038f602efc39f0a488543
totobill <antho.rohr@yahoo.fr>
2017-11-10 11:34:56 +0100

    modification liens js

--- data/genere/dev-cados/layout/bootstrap.php
+++ data/genere/dev-cados/layout/bootstrap.php
@@ -10,8 +10,8 @@
     <!-- Bootstrap -->
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
-    <script type="text/javascript" src="https://maps.google.com/maps/api/js?sensor=true"></script>
+<!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+    <script type="text/javascript" src="https://maps.google.com/maps/api/js?sensor=true"></script>-->
     <script src="js/gmaps.js"></script>
     <link rel="stylesheet" href="css/test.css" />
   </head>
